e4e7584562b266fe7a5ab36a8eef603e88448ee1
PravinkumarDs-2 <mishrapravin2702@gmail.com>
2026-08-19 09:23:00 +0530

    these are fsd program

---
+++ multiplication.js
@@ -0,0 +1,9 @@
+function multiply (a,b){
+return a*b ;}
+
+let num1=prompt("enter first number");
+let num2=prompt("enter second number");
+    num1=Number(num1);
+    num2=Number(num2);
+let result= multiply(num1,num2);
+console.log(result);    
